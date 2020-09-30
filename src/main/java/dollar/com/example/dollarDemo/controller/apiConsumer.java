de4b0ce23e08d16816085bafe4c14203573671bc
ricardotosin <ricardo.tosin@exxonmobil.com>
2020-09-30 15:24:49 -0300

    getting url date parameter to use in the get method to Central Bank API

--- src/main/java/dollar/com/example/dollarDemo/controller/apiConsumer.java
+++ src/main/java/dollar/com/example/dollarDemo/controller/apiConsumer.java
@@ -2,27 +2,42 @@ package dollar.com.example.dollarDemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import java.util.Arrays;
 
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Optional;
+@ResponseBody
 @RestController
 public class apiConsumer {
   @Autowired
   RestTemplate restTemplate;
+  @RequestMapping("/api/getdollar/{date}")
+  public Serializable getDollarByDate(@PathVariable("date") String date) {
 
-  @RequestMapping("/api/getdollar")
-  public String getDollarByDate() {
-    String url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='11-27-2017'&$format=json";
+    String DataUrl = "";
+    String url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='"+date+"'&$format=json";
+
     HttpHeaders headers = new HttpHeaders();
     headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
     HttpEntity<String> entity = new HttpEntity<String>(headers);
-    return restTemplate.exchange(url, HttpMethod.GET, entity, String.class).getBody();
+
+    ResponseEntity<String> response = restTemplate.exchange(
+        url,
+        HttpMethod.GET,
+        entity,
+        String.class
+    );
+
+    if (response.getStatusCode() == HttpStatus.OK) {
+      System.out.println("Request Successful.");
+      return(response.getBody());
+    } else {
+      System.out.println("Request Failed");
+      return(response.getStatusCode());
+    }
   }
 }
-
